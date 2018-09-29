84a122ad83e9fea81bf1f1a14d4bd26712fa80df
beepscore <support@beepscore.com>
2018-09-28 17:59:09 -0700

    implement __iter__ and __next__  I needed to research how to implement __iter__ and __next__ Start implementing generate_sales_report

--- csv_parser.py
+++ csv_parser.py
@@ -13,15 +13,18 @@ class CsvParser(object):
         """
         # TODO: Write this method body.
         self.data = data
-        
-        # can get from file number of lines
-        # number_of_weeks = 52
-        
-        # get number of products
-        header_cols = line0.split(',').strip()
+
+        lines = self.data.split('\n')
+        self.header_line = lines[0]
+        self.week_lines = lines[1:]
+
+        header_cols = self.header_line.split(',')
+        header_cols = [element.strip() for element in header_cols]
         self.product_cols = header_cols[1:]
-        number_of_products = len(self.product_cols)
-        
+        self.number_of_products = len(self.product_cols)
+
+        self.week_index = 0
+
     def __iter__(self):
         """
         Allows this object to act like an iterator
@@ -30,21 +33,33 @@ class CsvParser(object):
               # Use the array representing that CSV record
         """
         # TODO: Write this method body.
-        
-        for row in self.data.readlines():
-            yield next(row)
 
-    def next(self, row):
+        return self
+
+    def __next__(self):
         """
         Returns the next line from a CSV file, as an array, with one
         column per array element (where each element is string).
         :row: str e.g. '0,568.15,180.12,513.40...'
         :return: list of str e.g. ['0','568.15'...]
+
+        I needed to research how to implement __iter__ and __next__
+        https://stackoverflow.com/questions/4019971/how-to-implement-iter-self-for-a-container-object-python#4019987
+        https://docs.python.org/3/tutorial/classes.html#generators
+        https://stackoverflow.com/questions/19151/build-a-basic-python-iterator#24377
+        https://stackoverflow.com/questions/40255096/next-in-generators-and-iterators-and-what-is-a-method-wrapper
         """
         # TODO: Write this method body.
 
-        if row is None:
-            return None
-        
-        row_list = row.split(',').strip()
-        return row_list
+        try:
+            week_line = self.week_lines[self.week_index]
+            week_list = week_line.split(',')
+            week_list = [element.strip() for element in week_list]
+        except IndexError:
+            raise StopIteration()
+
+        self.week_index += 1
+        return week_list
+
+
+
--- sales_report.py
+++ sales_report.py
@@ -6,8 +6,17 @@ from csv_parser import CsvParser
 class SalesReport:
     """
     This class holds the data returned by your generate_sales_report() method.
+
+    # Your SalesReport must also contain:
+    # * The total sales associated with each product over the quarter.
+    # * The average weekly sales associated with each product.
+    # * Products should be indexed by product name in the report.
+
     """
     # TODO: Write this class body.
+
+    def __init__(self, ):
+        self.weekly_sales = []
 
 
 def generate_sales_report(parser):
@@ -22,42 +31,45 @@ def generate_sales_report(parser):
        1      581.34    312.01    480.02
        ...
        11      545.34    134.62    502.10
+
     For each week, we display the sales generated from each of N products
-    represented in the second through N+1th columns of the CSV. The 1st
-    column indicates the week number.
-    Each quarter consists of 12 weeks. This method generates
-    a sales report with the following aggregate data:
+    represented in the second through N+1th columns of the CSV.
+    The 1st column indicates the week number.
+    Each quarter consists of 12 weeks.
+    This method generates a sales report with the following aggregate data:
     * The total value associated with each week.
     * Identify the week with the highest sales.
     @param parser a CsvParser initialized with the input CSV data.
     @return a SalesReport containing the figures of merit described above.
-    
-    
+
             Product1  Product2  Product3 ...
     total     568.15    180.12    513.40
     
     """
 
-
-
-
-
-
-
-    report = SalesReport()
-
     # TODO: Write this method body.
-    return report
+
+    salesReport = SalesReport()
+
+    product_sums = [0] * parser.number_of_products
+    NUMBER_OF_WEEKS_IN_QUARTER = 12
+    weekly_sums = [0] * NUMBER_OF_WEEKS_IN_QUARTER
+    print(product_sums)
+
+    # iterate through parser
+    for csv_line_as_array in parser:
+
+        for product_index in range(parser.number_of_products):
+            # TODO: currency, use Decimal not float
+            product_sales = float(csv_line_as_array[product_index + 1])
+            product_sums[product_index] += product_sales
+
+    report_text = str(parser.header_line) + '\n' + str(product_sums)
 
 
 if __name__ == '__main__':
 
     print("Test")
-
-    # Your SalesReport must also contain:
-    # * The total sales associated with each product over the quarter.
-    # * The average weekly sales associated with each product.
-    # * Products should be indexed by product name in the report.
 
     # sales_text contains entire csv file
     filename = './data/sales.csv'
@@ -66,4 +78,7 @@ if __name__ == '__main__':
 
     # print(sales_text)
     parser = CsvParser(sales_text)
+
+    generate_sales_report(parser)
+
 
